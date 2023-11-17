0fd80f90ce118bdca96f0985859ef0bdd664fec6
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2023-11-17 10:09:18 +0400

    cart details done

--- ECommerce.WebUI/Controllers/CartController.cs
+++ ECommerce.WebUI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.Business.Abstract;
+using ECommerce.WebUI.Models;
 using ECommerce.WebUI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,7 +18,7 @@ namespace ECommerce.WebUI.Controllers
             _cartService = cartService;
         }
 
-        public async Task<IActionResult> AddToCart(int productId)
+        public async Task<IActionResult> AddToCart(int productId,int page,int category)
         {
             var productToBeAdded = await _productService.GetById(productId);
             var cart = _cartSessionService.GetCart();
@@ -27,7 +28,27 @@ namespace ECommerce.WebUI.Controllers
 
             TempData.Add("message", String.Format("Your product, {0} was added successfully to cart", productToBeAdded.ProductName));
 
-            return RedirectToAction("Index", "Product");
+            return RedirectToAction("Index", "Product",new {page=page,category=category});
+        }
+
+        public IActionResult List()
+        {
+            var cart = _cartSessionService.GetCart();
+            var model = new CartListViewModel
+            {
+                Cart=cart
+            };
+            return View(model);
+        }
+
+        public IActionResult Remove(int productId)
+        {
+            var cart=_cartSessionService.GetCart();
+
+            _cartService.RemoveFromCart(cart, productId);
+            _cartSessionService.SetCart(cart);
+            TempData.Add("message", "Your Product was removed successfully from cart");
+            return RedirectToAction("List");
         }
     }
 }
